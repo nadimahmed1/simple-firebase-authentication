7a917550a6b5b3b88c961e45fc97a8849a50f764
nadimahmed1 <nadimarafat26@gmail.com>
2022-10-18 08:12:36 +0600

    sign out added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
+import { useState } from 'react';
 
 const auth = getAuth(app)
 
 function App() {
+  const [user, setUser] = useState({})
+
   const provider = new GoogleAuthProvider();
+
   const handleGoogleSignIn = () => {
+
     signInWithPopup(auth, provider)
       .then(result => {
         const user = result.user
+        setUser(user)
         console.log(user);
       })
       .catch(error => {
         console.log('error', error);
       })
   }
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({})
+      })
+      .catch(error => {
+        setUser({});
+      })
+  }
+
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      {/* {
+        condition ? true : false
+      } */}
+      {
+        user.email ? <button onClick={handleSignOut}>Sing Out</button> : <button onClick={handleGoogleSignIn}>Google Sign In</button>
+
+      }
+
+      <h2>Name: {user.displayName}</h2>
+      <p>I know your email address: {user.email}</p>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
